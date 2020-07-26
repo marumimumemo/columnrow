fdf569e71ac1620b5707a5c6697ab7421aac6d84
marumimumemo <sagaminumber3@yahoo.co.jp>
2020-07-26 18:55:41 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Column & Row',
+      home: Container(
+        child: Row(
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                Container( color: Colors.blue, width: 100, height:100 ),
+                Container( color: Colors.red, width: 100, height:100 ),
+              ],
+            ),
+            Column(
+              children: <Widget>[
+                Container( color: Colors.green, width: 100, height:100 ),
+                Container( color: Colors.orange, width: 100, height:100 ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
